266da359f28ea6fe20be1bb29315f65abb1561b9
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-11-04 16:38:43 -0400

    Set up server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./config/db');
+const routes = require('./controllers');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
