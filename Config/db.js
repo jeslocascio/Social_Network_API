9917545c5064cde6b2250bc6b9f42d6fd8f120d5
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-11-04 16:35:57 -0400

    Fixed Errors in Database file

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const db = process.env.MONGODB_URI || 'mongodb://localhost/social-network-api';
+const db = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api';
 
 connect(db);
 
